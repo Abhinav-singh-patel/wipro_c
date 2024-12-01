b94718323ab2f069536c89712e72d95507fa5ce0
Abhinav-singh-patel <patelabhinavvns@gmail.com>
2024-12-01 13:07:18 +0530

    upload

---
+++ Assignment/p14.c
@@ -0,0 +1,19 @@
+// a character is an alphabet
+
+#include <stdio.h>
+
+int main() {
+    char ch;
+
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    // Check if the character is an alphabet using ASCII values
+    if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+        printf("%c is an alphabet.\n", ch);
+    } else {
+        printf("%c is not an alphabet.\n", ch);
+    }
+
+    return 0;
+}
